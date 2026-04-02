593ff6099f8895151f9bbeb1e1e3da8f56cc4425
ankushh1196-cloud <ankushh1196@gmail.com>
2026-04-02 12:38:48 +0530

    Update urls.py

--- mindbot/chat/urls.py
+++ mindbot/chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home),
-    path('chatbot/',path('create-admin/', views.create_admin), views.chatbot),
+    path('', views.home, name='home'),
+    path('chatbot/', views.chatbot, name='chatbot'),
+    path('create-admin/', views.create_admin, name='create_admin'),
 ]
